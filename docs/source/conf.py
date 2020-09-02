fce620bef35ac84beba639cb1c922ea470dd2335
Jean Begaint <Jean.Begaint@interdigital.com>
2020-09-02 09:47:17 -0700

    Apply formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,13 +27,13 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../compressai/'))
+sys.path.insert(0, os.path.abspath("../compressai/"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'compressai'
-copyright = '2020, InterDigital Communications, Inc.'
-author = 'InterDigital Communications, Inc.'
+project = "compressai"
+copyright = "2020, InterDigital Communications, Inc."
+author = "InterDigital Communications, Inc."
 
 # -- General configuration ---------------------------------------------------
 
@@ -41,30 +41,30 @@ author = 'InterDigital Communications, Inc.'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
