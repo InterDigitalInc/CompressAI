579a2968dbd16dabe86bc490461ae6242386a81e
Jean Begaint <Jean.Begaint@interdigital.com>
2020-12-23 08:51:24 -0800

    Add missing clamp to FactorizedPrior decompress

--- compressai/models/priors.py
+++ compressai/models/priors.py
@@ -195,7 +195,7 @@ class FactorizedPrior(CompressionModel):
     def decompress(self, strings, shape):
         assert isinstance(strings, list) and len(strings) == 1
         y_hat = self.entropy_bottleneck.decompress(strings[0], shape)
-        x_hat = self.g_s(y_hat)
+        x_hat = self.g_s(y_hat).clamp_(0, 1)
         return {"x_hat": x_hat}
 
 
