ef49662cdb76866e37b13baa7fd92049de371bde
Jean Begaint <Jean.Begaint@interdigital.com>
2021-05-05 10:18:59 -0700

    Dev 1.1.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 cwd = Path(__file__).resolve().parent
 
 package_name = "compressai"
-version = "1.1.4"
+version = "1.1.5dev0"
 git_hash = "unknown"
 
 
