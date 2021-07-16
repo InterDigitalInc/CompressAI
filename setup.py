70ac2fa282e2b991681e3f73588e647768b29f17
Fabien Racape <fabien.racape@interdigital.com>
2021-07-16 10:07:42 -0700

    Dev 1.1.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 cwd = Path(__file__).resolve().parent
 
 package_name = "compressai"
-version = "1.1.6"
+version = "1.1.7dev0"
 git_hash = "unknown"
 
 
