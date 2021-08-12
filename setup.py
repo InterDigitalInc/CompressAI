3cf967470f9c34f9704b6ef5ad6ed2b879aa4c57
Jean Begaint <Jean.Begaint@interdigital.com>
2021-08-11 18:53:43 -0700

    Build and tests for python 3.9

--- setup.py
+++ setup.py
@@ -132,6 +132,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     ext_modules=get_extensions(),
