11be7c18d265c8bd9c387801fb13481789769649
Fabien Racape <fabien.racape@interdigital.com>
2021-09-27 22:59:00 -0700

    add VTM

--- compressai/utils/video/bench/codecs.py
+++ compressai/utils/video/bench/codecs.py
@@ -1,10 +1,27 @@
 import abc
 import argparse
+import platform
+import os
+import subprocess
+import sys
+import time
 
 from pathlib import Path
 from typing import Any, List
 
 from compressai.datasets.rawvideo import get_raw_video_file_info
+
+
+def run_command(cmd, ignore_returncodes=None):
+    cmd = [str(c) for c in cmd]
+    try:
+        rv = subprocess.check_output(cmd)
+        return rv.decode("ascii")
+    except subprocess.CalledProcessError as err:
+        if ignore_returncodes is not None and err.returncode in ignore_returncodes:
+            return err.output
+        print(err.output.decode("utf-8"))
+        sys.exit(1)
 
 
 class Codec(abc.ABC):
@@ -92,3 +109,52 @@ class x265(x264):
             outputpath,
         ]
         return cmd
+
+
+class x265(x264):
+    name = "x265"
+
+    def get_encode_cmd(self, filepath: Path, **args: Any) -> List[Any]:
+        info = get_raw_video_file_info(filepath.stem)
+        outputpath = self.get_output_path(filepath, **args)
+        cmd = [
+            "ffmpeg",
+            "-s:v",
+            f"{info['width']}x{info['height']}",
+            "-i",
+            filepath,
+            "-c:v",
+            "hevc",
+            "-crf",
+            args["qp"],
+            "-preset",
+            args["preset"],
+            "-x265-params",
+            "bframes=0",
+            "-tune",
+            "ssim",
+            "-pix_fmt",
+            "yuv420p",
+            "-threads",
+            "4",
+            outputpath,
+        ]
+        return cmd
+
+
+def get_vtm_encoder_path(build_dir):
+    system = platform.system()
+    try:
+        elfnames = {"Darwin": "EncoderApp", "Linux": "EncoderAppStatic"}
+        return os.path.join(build_dir, elfnames[system])
+    except KeyError as err:
+        raise RuntimeError(f'Unsupported platform "{system}"') from err
+
+
+def get_vtm_decoder_path(build_dir):
+    system = platform.system()
+    try:
+        elfnames = {"Darwin": "DecoderApp", "Linux": "DecoderAppStatic"}
+        return os.path.join(build_dir, elfnames[system])
+    except KeyError as err:
+        raise RuntimeError(f'Unsupported platform "{system}"') from err
