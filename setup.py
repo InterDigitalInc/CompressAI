f97a898e58898a31bd54541204809bdef179a06a
Fabien Racape <fabien.racape@interdigital.com>
2021-11-19 12:38:35 -0800

    Release version 1.1.9

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 cwd = Path(__file__).resolve().parent
 
 package_name = "compressai"
-version = "1.1.9.dev0"
+version = "1.1.9"
 git_hash = "unknown"
 
 
