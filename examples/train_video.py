Fix distortion computation.
Sum of computed metric values is dividied by the number of channels

examples/train_video.py
@@ -93,6 +93,10 @@ class RateDistortionLoss(nn.Module):
         if not len(x) == len(target):
             raise RuntimeError(f"len(x)={len(x)} != len(target)={len(target)})")
 
+        nC = x.size(1)
+        if not nC == target.size(1):
+            raise RuntimeError("number of channels mismatches while computing distortion")
+
         if isinstance(x, torch.Tensor):
             x = x.chunk(x.size(1), dim=1)
 
@@ -109,7 +113,7 @@ class RateDistortionLoss(nn.Module):
         metric_values = torch.stack(metric_values)
 
         # sum value over the components dimension
-        metric_value = torch.sum(metric_values.transpose(1, 0), dim=1)
+        metric_value = torch.sum(metric_values.transpose(1, 0), dim=1)/nC
         scaled_metric = self._scaling_functions(metric_value)
 
         return scaled_metric, metric_value