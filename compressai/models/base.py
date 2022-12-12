0fb253363475a78c5a1506c878d6ae692d89d004
Mateen Ulhaq <mateen.ulhaq@interdigital.com>
2022-12-11 17:50:02 -0800

    docs: fix formatting for aux_loss description

--- compressai/models/base.py
+++ compressai/models/base.py
@@ -113,30 +113,32 @@ class CompressionModel(nn.Module):
         return updated
 
     def aux_loss(self) -> Tensor:
-        """Returns the total auxiliary loss over all `EntropyBottleneck`s.
+        r"""Returns the total auxiliary loss over all ``EntropyBottleneck``\s.
 
         In contrast to the primary "net" loss used by the "net"
         optimizer, the "aux" loss is only used by the "aux" optimizer to
-        update *only* the `EntropyBottleneck.quantiles` parameters. In
+        update *only* the ``EntropyBottleneck.quantiles`` parameters. In
         fact, the "aux" loss does not depend on image data at all.
 
         The purpose of the "aux" loss is to determine the range within
         which most of the mass of a given distribution is contained, as
         well as its median (i.e. 50% probability). That is, for a given
         distribution, the "aux" loss converges towards satisfying the
-        following conditions for some chosen `tail_mass` probability:
-        - `cdf(quantiles[0]) = tail_mass / 2`,
-        - `cdf(quantiles[1]) = 0.5`, and
-        - `cdf(quantiles[2]) = 1 - tail_mass / 2`.
-        This ensures that the concrete `_quantized_cdf`s operate
+        following conditions for some chosen ``tail_mass`` probability:
+
+        * ``cdf(quantiles[0]) = tail_mass / 2``
+        * ``cdf(quantiles[1]) = 0.5``
+        * ``cdf(quantiles[2]) = 1 - tail_mass / 2``
+
+        This ensures that the concrete ``_quantized_cdf``\s operate
         primarily within a finitely supported region. Any symbols
         outside this range must be coded using some alternative method
-        that does *not* involve the `_quantized_cdf`s. Luckily, one may
-        choose a `tail_mass` probability that is sufficiently small so
-        that this rarely occurs. It is important that we work with
-        `_quantized_cdf`s that have a small finite support; otherwise,
-        entropy coding runtime performance would suffer. Thus,
-        `tail_mass` should not be too small, either!
+        that does *not* involve the ``_quantized_cdf``\s. Luckily, one
+        may choose a ``tail_mass`` probability that is sufficiently
+        small so that this rarely occurs. It is important that we work
+        with ``_quantized_cdf``\s that have a small finite support;
+        otherwise, entropy coding runtime performance would suffer.
+        Thus, ``tail_mass`` should not be too small, either!
         """
         loss = sum(m.loss() for m in self.modules() if isinstance(m, EntropyBottleneck))
         return cast(Tensor, loss)
