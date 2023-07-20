459683fa9f8ad8345c4bf99137dde6b25e9e6d65
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-20 16:01:56 -0700

    feat(models): add ELIC
    Context model from [He2022].
    
    [He2022]: `"ELIC: Efficient Learned Image Compression with
    Unevenly Grouped Space-Channel Contextual Adaptive Coding"
    <https://arxiv.org/abs/2203.10886>`_, by Dailan He, Ziming Yang,
    Weikun Peng, Rui Ma, Hongwei Qin, and Yan Wang, CVPR 2022.

--- compressai/models/sensetime.py
+++ compressai/models/sensetime.py
@@ -27,29 +27,34 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from itertools import accumulate
+
 import torch.nn as nn
 
 from compressai.entropy_models import EntropyBottleneck
 from compressai.latent_codecs import (
+    ChannelGroupsLatentCodec,
     CheckerboardLatentCodec,
     GaussianConditionalLatentCodec,
     HyperLatentCodec,
     HyperpriorLatentCodec,
 )
 from compressai.layers import (
+    CheckerboardMaskedConv2d,
     ResidualBlock,
     ResidualBlockUpsample,
     ResidualBlockWithStride,
     conv3x3,
+    sequential_channel_ramp,
     subpel_conv3x3,
 )
-from compressai.layers.layers import CheckerboardMaskedConv2d
 from compressai.registry import register_model
 
 from .base import SimpleVAECompressionModel
 
 __all__ = [
     "Cheng2020AnchorCheckerboard",
+    "Cheng2020AnchorElic",
 ]
 
 
@@ -139,6 +144,143 @@ class Cheng2020AnchorCheckerboard(SimpleVAECompressionModel):
                     context_prediction=CheckerboardMaskedConv2d(
                         N, 2 * N, kernel_size=5, padding=2, stride=1
                     ),
+                ),
+                "hyper": HyperLatentCodec(
+                    entropy_bottleneck=EntropyBottleneck(N), h_a=h_a, h_s=h_s
+                ),
+            },
+        )
+
+    @classmethod
+    def from_state_dict(cls, state_dict):
+        """Return a new model instance from `state_dict`."""
+        N = state_dict["g_a.0.conv1.weight"].size(0)
+        net = cls(N)
+        net.load_state_dict(state_dict)
+        return net
+
+
+@register_model("cheng2020-anchor-elic")
+class Cheng2020AnchorElic(SimpleVAECompressionModel):
+    """Cheng2020 anchor model with checkerboard context model.
+
+    Base transform model from [Cheng2020]. Context model from [He2022].
+
+    [Cheng2020]: `"Learned Image Compression with Discretized Gaussian
+    Mixture Likelihoods and Attention Modules"
+    <https://arxiv.org/abs/2001.01568>`_, by Zhengxue Cheng, Heming Sun,
+    Masaru Takeuchi, and Jiro Katto, CVPR 2020.
+
+    [He2022]: `"ELIC: Efficient Learned Image Compression with
+    Unevenly Grouped Space-Channel Contextual Adaptive Coding"
+    <https://arxiv.org/abs/2203.10886>`_, by Dailan He, Ziming Yang,
+    Weikun Peng, Rui Ma, Hongwei Qin, and Yan Wang, CVPR 2022.
+
+    Uses residual blocks with small convolutions (3x3 and 1x1), and sub-pixel
+    convolutions for up-sampling.
+
+    Args:
+        N (int): Number of channels
+        groups (list[int]): Number of channels in each channel group
+    """
+
+    def __init__(self, N=192, groups=None, **kwargs):
+        super().__init__(**kwargs)
+
+        if groups is None:
+            groups = [16, 16, 32, 64, 64]
+
+        assert sum(groups) == N
+        self.groups = list(groups)
+        self.groups_acc = list(accumulate(self.groups, initial=0))
+
+        self.g_a = nn.Sequential(
+            ResidualBlockWithStride(3, N, stride=2),
+            ResidualBlock(N, N),
+            ResidualBlockWithStride(N, N, stride=2),
+            ResidualBlock(N, N),
+            ResidualBlockWithStride(N, N, stride=2),
+            ResidualBlock(N, N),
+            conv3x3(N, N, stride=2),
+        )
+
+        self.g_s = nn.Sequential(
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            ResidualBlock(N, N),
+            ResidualBlockUpsample(N, N, 2),
+            ResidualBlock(N, N),
+            subpel_conv3x3(N, 3, 2),
+        )
+
+        h_a = nn.Sequential(
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N, stride=2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N, stride=2),
+        )
+
+        h_s = nn.Sequential(
+            conv3x3(N, N),
+            nn.LeakyReLU(inplace=True),
+            subpel_conv3x3(N, N, 2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N, N * 3 // 2),
+            nn.LeakyReLU(inplace=True),
+            subpel_conv3x3(N * 3 // 2, N * 3 // 2, 2),
+            nn.LeakyReLU(inplace=True),
+            conv3x3(N * 3 // 2, N * 2),
+        )
+
+        self.latent_codec = HyperpriorLatentCodec(
+            latent_codec={
+                "y": ChannelGroupsLatentCodec(
+                    groups=self.groups,
+                    channel_context={
+                        f"y{k}": sequential_channel_ramp(
+                            self.groups_acc[k],
+                            self.groups[k] * 2,
+                            num_layers=3,
+                            make_layer=nn.Conv2d,
+                            make_act=lambda: nn.ReLU(inplace=True),
+                            kernel_size=5,
+                            padding=2,
+                            stride=1,
+                        )
+                        for k in range(1, len(self.groups))
+                    },
+                    latent_codec={
+                        f"y{k}": CheckerboardLatentCodec(
+                            latent_codec={
+                                "y": GaussianConditionalLatentCodec(quantizer="ste"),
+                            },
+                            entropy_parameters=sequential_channel_ramp(
+                                N * 2 + self.groups[k] * (2 if k == 0 else 4),
+                                self.groups[k] * 2,
+                                num_layers=3,
+                                make_layer=nn.Conv2d,
+                                make_act=lambda: nn.ReLU(inplace=True),
+                                kernel_size=1,
+                                padding=0,
+                                stride=1,
+                            ),
+                            context_prediction=CheckerboardMaskedConv2d(
+                                self.groups[k],
+                                self.groups[k] * 2,
+                                kernel_size=5,
+                                padding=2,
+                                stride=1,
+                            ),
+                        )
+                        for k in range(len(self.groups))
+                    },
                 ),
                 "hyper": HyperLatentCodec(
                     entropy_bottleneck=EntropyBottleneck(N), h_a=h_a, h_s=h_s
