4c33f0e3537f1aa9a5b5ad1ac2c74661b840fead
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-09-29 06:21:44 -0700

    feat: elic2022 use official architecture recommendations
    See figures 5, 6, 7 of [He2022] ELIC paper.

--- compressai/models/sensetime.py
+++ compressai/models/sensetime.py
@@ -27,9 +27,10 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from itertools import accumulate
-
+import torch
 import torch.nn as nn
+
+from torch import Tensor
 
 from compressai.entropy_models import EntropyBottleneck
 from compressai.latent_codecs import (
@@ -40,10 +41,12 @@ from compressai.latent_codecs import (
     HyperpriorLatentCodec,
 )
 from compressai.layers import (
+    AttentionBlock,
     CheckerboardMaskedConv2d,
     ResidualBlock,
     ResidualBlockUpsample,
     ResidualBlockWithStride,
+    conv1x1,
     conv3x3,
     sequential_channel_ramp,
     subpel_conv3x3,
@@ -51,10 +54,11 @@ from compressai.layers import (
 from compressai.registry import register_model
 
 from .base import SimpleVAECompressionModel
+from .utils import conv, deconv
 
 __all__ = [
     "Cheng2020AnchorCheckerboard",
-    "Cheng2020AnchorElic",
+    "Elic2022Official",
 ]
 
 
@@ -142,7 +146,7 @@ class Cheng2020AnchorCheckerboard(SimpleVAECompressionModel):
                         nn.Conv2d(N * 8 // 3, N * 6 // 3, 1),
                     ),
                     context_prediction=CheckerboardMaskedConv2d(
-                        N, 2 * N, kernel_size=5, padding=2, stride=1
+                        N, 2 * N, kernel_size=5, stride=1, padding=2
                     ),
                 ),
                 "hyper": HyperLatentCodec(
@@ -160,96 +164,101 @@ class Cheng2020AnchorCheckerboard(SimpleVAECompressionModel):
         return net
 
 
-@register_model("cheng2020-anchor-elic")
-class Cheng2020AnchorElic(SimpleVAECompressionModel):
-    """Cheng2020 anchor model with checkerboard context model.
+@register_model("elic2022-official")
+class Elic2022Official(SimpleVAECompressionModel):
+    """ELIC 2022; uneven channel groups with checkerboard spatial context.
 
-    Base transform model from [Cheng2020]. Context model from [He2022].
-
-    [Cheng2020]: `"Learned Image Compression with Discretized Gaussian
-    Mixture Likelihoods and Attention Modules"
-    <https://arxiv.org/abs/2001.01568>`_, by Zhengxue Cheng, Heming Sun,
-    Masaru Takeuchi, and Jiro Katto, CVPR 2020.
+    Context model from [He2022].
+    Based on modified attention model architecture from [Cheng2020].
 
     [He2022]: `"ELIC: Efficient Learned Image Compression with
     Unevenly Grouped Space-Channel Contextual Adaptive Coding"
     <https://arxiv.org/abs/2203.10886>`_, by Dailan He, Ziming Yang,
     Weikun Peng, Rui Ma, Hongwei Qin, and Yan Wang, CVPR 2022.
 
-    Uses residual blocks with small convolutions (3x3 and 1x1), and sub-pixel
-    convolutions for up-sampling.
+    [Cheng2020]: `"Learned Image Compression with Discretized Gaussian
+    Mixture Likelihoods and Attention Modules"
+    <https://arxiv.org/abs/2001.01568>`_, by Zhengxue Cheng, Heming Sun,
+    Masaru Takeuchi, and Jiro Katto, CVPR 2020.
 
     Args:
-        N (int): Number of channels
+        N (int): Number of main network channels
+        M (int): Number of latent space channels
         groups (list[int]): Number of channels in each channel group
     """
 
-    def __init__(self, N=192, groups=None, **kwargs):
+    def __init__(self, N=192, M=320, groups=None, **kwargs):
         super().__init__(**kwargs)
 
         if groups is None:
-            groups = [16, 16, 32, 64, 64]
+            groups = [16, 16, 32, 64, M - 128]
 
-        assert sum(groups) == N
         self.groups = list(groups)
-        self.groups_acc = list(accumulate(self.groups, initial=0))
+        assert sum(self.groups) == M
 
         self.g_a = nn.Sequential(
-            ResidualBlockWithStride(3, N, stride=2),
-            ResidualBlock(N, N),
-            ResidualBlockWithStride(N, N, stride=2),
-            ResidualBlock(N, N),
-            ResidualBlockWithStride(N, N, stride=2),
-            ResidualBlock(N, N),
-            conv3x3(N, N, stride=2),
+            conv(3, N, kernel_size=5, stride=2),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            conv(N, N, kernel_size=5, stride=2),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            AttentionBlock(N),
+            conv(N, N, kernel_size=5, stride=2),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            conv(N, M, kernel_size=5, stride=2),
+            AttentionBlock(M),
         )
 
         self.g_s = nn.Sequential(
-            ResidualBlock(N, N),
-            ResidualBlockUpsample(N, N, 2),
-            ResidualBlock(N, N),
-            ResidualBlockUpsample(N, N, 2),
-            ResidualBlock(N, N),
-            ResidualBlockUpsample(N, N, 2),
-            ResidualBlock(N, N),
-            subpel_conv3x3(N, 3, 2),
+            AttentionBlock(M),
+            deconv(M, N, kernel_size=5, stride=2),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            deconv(N, N, kernel_size=5, stride=2),
+            AttentionBlock(N),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            deconv(N, N, kernel_size=5, stride=2),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            ResidualBottleneckBlock(N, N),
+            deconv(N, 3, kernel_size=5, stride=2),
         )
 
         h_a = nn.Sequential(
-            conv3x3(N, N),
-            nn.LeakyReLU(inplace=True),
-            conv3x3(N, N),
-            nn.LeakyReLU(inplace=True),
-            conv3x3(N, N, stride=2),
-            nn.LeakyReLU(inplace=True),
-            conv3x3(N, N),
-            nn.LeakyReLU(inplace=True),
-            conv3x3(N, N, stride=2),
+            conv(M, N, kernel_size=3, stride=1),
+            nn.ReLU(inplace=True),
+            conv(N, N, kernel_size=5, stride=2),
+            nn.ReLU(inplace=True),
+            conv(N, N, kernel_size=5, stride=2),
         )
 
         h_s = nn.Sequential(
-            conv3x3(N, N),
-            nn.LeakyReLU(inplace=True),
-            subpel_conv3x3(N, N, 2),
-            nn.LeakyReLU(inplace=True),
-            conv3x3(N, N * 3 // 2),
-            nn.LeakyReLU(inplace=True),
-            subpel_conv3x3(N * 3 // 2, N * 3 // 2, 2),
-            nn.LeakyReLU(inplace=True),
-            conv3x3(N * 3 // 2, N * 2),
+            deconv(N, N, kernel_size=5, stride=2),
+            nn.ReLU(inplace=True),
+            deconv(N, N * 3 // 2, kernel_size=5, stride=2),
+            nn.ReLU(inplace=True),
+            deconv(N * 3 // 2, N * 2, kernel_size=3, stride=1),
         )
 
         # In [He2022], this is labeled "g_ch^(k)".
         channel_context = {
             f"y{k}": sequential_channel_ramp(
-                self.groups_acc[k],
+                sum(self.groups[:k]),
                 self.groups[k] * 2,
                 num_layers=3,
                 make_layer=nn.Conv2d,
                 make_act=lambda: nn.ReLU(inplace=True),
                 kernel_size=5,
-                padding=2,
                 stride=1,
+                padding=2,
             )
             for k in range(1, len(self.groups))
         }
@@ -260,8 +269,8 @@ class Cheng2020AnchorElic(SimpleVAECompressionModel):
                 self.groups[k],
                 self.groups[k] * 2,
                 kernel_size=5,
-                padding=2,
                 stride=1,
+                padding=2,
             )
             for k in range(len(self.groups))
         ]
@@ -269,14 +278,15 @@ class Cheng2020AnchorElic(SimpleVAECompressionModel):
         # In [He2022], this is labeled "Param Aggregation".
         param_aggregation = [
             sequential_channel_ramp(
-                N * 2 + self.groups[k] * (2 if k == 0 else 4),
+                # Input: spatial context, channel context, and hyper params.
+                self.groups[k] * 2 + (k > 0) * self.groups[k] * 2 + N * 2,
                 self.groups[k] * 2,
                 num_layers=3,
                 make_layer=nn.Conv2d,
                 make_act=lambda: nn.ReLU(inplace=True),
                 kernel_size=1,
-                padding=0,
                 stride=1,
+                padding=0,
             )
             for k in range(len(self.groups))
         ]
@@ -313,7 +323,50 @@ class Cheng2020AnchorElic(SimpleVAECompressionModel):
     @classmethod
     def from_state_dict(cls, state_dict):
         """Return a new model instance from `state_dict`."""
-        N = state_dict["g_a.0.conv1.weight"].size(0)
+        N = state_dict["g_a.0.weight"].size(0)
         net = cls(N)
         net.load_state_dict(state_dict)
         return net
+
+
+class ResidualBottleneckBlock(nn.Module):
+    """Residual bottleneck block.
+
+    Introduced by [He2016], this block sandwiches a 3x3 convolution
+    between two 1x1 convolutions which reduce and then restore the
+    number of channels. This reduces the number of parameters required.
+
+    [He2016]: `"Deep Residual Learning for Image Recognition"
+    <https://arxiv.org/abs/1512.03385>`_, by Kaiming He, Xiangyu Zhang,
+    Shaoqing Ren, and Jian Sun, CVPR 2016.
+
+    Args:
+        in_ch (int): Number of input channels
+        out_ch (int): Number of output channels
+    """
+
+    def __init__(self, in_ch: int, out_ch: int):
+        super().__init__()
+
+        mid_ch = min(in_ch, out_ch) // 2
+        self.conv1 = conv1x1(in_ch, mid_ch)
+        self.conv2 = conv3x3(mid_ch, mid_ch)
+        self.conv3 = conv1x1(mid_ch, out_ch)
+        self.relu = nn.ReLU(inplace=True)
+
+        if in_ch != out_ch:
+            self.skip = conv1x1(in_ch, out_ch)
+        else:
+            self.skip = None
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x if self.skip is None else self.skip(x)
+
+        out = x
+        out = self.conv1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+
+        return out + identity
