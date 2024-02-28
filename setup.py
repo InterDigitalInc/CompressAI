c2c5addfe3cd4d84b1f9e7d9b20c44f8116930f2
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-02-28 12:44:04 -0800

    chore(deps): pointops, pyntcloud use PyPI versions

--- setup.py
+++ setup.py
@@ -107,8 +107,8 @@ DEV_REQUIRES = TEST_REQUIRES + [
     "mypy",
 ]
 POINTCLOUD_REQUIRES = [
-    "pointops @ git+https://github.com/YodaEmbedding/pointops.git",
-    "pyntcloud @ git+https://github.com/YodaEmbedding/pyntcloud.git@12ee9f2208f4207844be80ac5fdbafaf9f0652fa",
+    "pointops-yoda",
+    "pyntcloud-yoda",  # Patched version of pyntcloud.
 ]
 
 
