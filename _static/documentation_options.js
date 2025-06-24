a4459bd7cbec9e41855793cae39adbd5659e442d
fracape <fracape@users.noreply.github.com>
2025-06-24 01:15:22 +0000

    deploy: c8dde40e7199d8a235db5204f0a6dec17131b02a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,13 +1,12 @@
-const DOCUMENTATION_OPTIONS = {
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '',
-    LANGUAGE: 'en',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '',
-    NAVIGATION_WITH_KEYS: false,
-    SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
+    NAVIGATION_WITH_KEYS: true
 };
